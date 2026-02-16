5c5ba352c7a8ec89d71545f5862b4d619f765d19
taufik69 <taufik35-2915@diu.edu.bd>
2026-02-16 17:12:13 +0600

    feat: add product model with monggose pre middleware

--- src/shared/routes/index.routes.js
+++ src/shared/routes/index.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const _ = express.Router();
 _.use("/categories", require("@/modules/categories/categories.routes"));
+_.use("/product", require("@/modules/product/product.routes"));
 
 module.exports = _;
